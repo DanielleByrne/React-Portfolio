9534540b5fedb7ec76d16898716108dff1373907
Danielle Byrne <DanielleByrne421@gmail.com>
2020-08-05 23:00:34 -0400

    set up initial react files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NoMatch from "./containers/NoMatch/NoMatch";
+import Home from "./containers/Home/Home";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path = "/" component= {Home} />
+        <Route component= {NoMatch}/>
+      </Switch>
+    </Router>
   );
 }
 
